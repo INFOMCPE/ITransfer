91590c9276783ab106d10f584c86835a40a1dad8
Aleksey Lozovagin <Oleksih@gmail.com>
2017-03-10 16:45:31 +0200

    Update to v2.0

--- src/infomcpe/ITransfer.php
+++ src/infomcpe/ITransfer.php
@@ -10,6 +10,7 @@ use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\utils\Config;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\utils\Utils; 
 use pocketmine\Server;
 use pocketmine\item\Item;
@@ -18,10 +19,13 @@ use pocketmine\tile\Sign;
 
 class ITransfer extends PluginBase implements Listener {
      const Prfix = '§f[§aITransfer§f]§e ';
-     
-   public function onEnable(){
+
+    private $last_x;
+
+    public function onEnable(){
         $this->saveDefaultConfig();
         @mkdir($this->getDataFolder().'signs');
+        @mkdir($this->getDataFolder().'portals');
                if(!file_exists($this->getDataFolder().'lang.json')){
                    $this->languageInitialization();
                }
@@ -67,6 +71,10 @@ class ITransfer extends PluginBase implements Listener {
                             $this->session->createSession(strtolower($sender->getName()), 'stat',3);
                             $sender->sendMessage(ITransfer::Prfix.$this->lang("press_send_1"));
                                 break;
+                             case 'addportal':
+                            $this->session->createSession(strtolower($sender->getName()), 'stat', 5);
+                            $sender->sendMessage(ITransfer::Prfix."Пожалуйста нажмите на первую точку портала");
+                                break;
                             case 'update':
                                 $this->signTask();
                                 break;
@@ -81,14 +89,40 @@ class ITransfer extends PluginBase implements Listener {
             $player = $event->getPlayer();
             $message = $event->getMessage();
             if($this->session->getSessionData(strtolower($player->getName()), 'stat') == 3){
-                
                 $this->session->createSession(strtolower($player->getName()), 'stat', 1);
                 $this->session->createSession(strtolower($player->getName()), 'ip', $message);
                 $player->sendMessage(ITransfer::Prfix.$this->lang("press_tap"));
                 $event->setCancelled(); 
                 }
+                if($this->session->getSessionData(strtolower($player->getName()), 'stat') == 7){
+                    $this->dataSave($message, 'min', $this->session->getSessionData(strtolower($player->getName()), 'min'), 'portals');
+                    $this->dataSave($message, 'max', $this->session->getSessionData(strtolower($player->getName()), 'max'), 'portals');
+                    $this->dataSave($message, 'ip', $message, 'portals');
+                    $player->sendMessage(ITransfer::Prfix.'Успешно. Портал добавлен');
+                            
+                }
                 
             
+        }
+        public function onJump(\pocketmine\event\server\DataPacketReceiveEvent $event){
+            $player = $event->getPlayer();
+            $pk = $event->getPacket();
+            if($pk instanceof \pocketmine\network\protocol\MovePlayerPacket  ){
+                if($pk->mode == 0){  
+            if ($this->last_x != round($player->getX())){
+                $this->last_x = round($player->getX());
+              foreach (glob($this->getDataFolder().'portals/*.json') as $filename) {
+                $data = json_decode(file_get_contents($filename), TRUE);
+                if($this->cheakXYZ($player->getX(), $player->getY(), $player->getZ(), $data['min'], $data['max']) == true){
+                    $this->transfer($data['ip'], $player);
+                    
+                }else{
+                 
+                }
+            }
+                }
+                }
+        }
         }
          public function onBlockBreak(BlockBreakEvent $event){
          $player = $event->getPlayer();
@@ -127,7 +161,43 @@ class ITransfer extends PluginBase implements Listener {
                             }
                    }
              }
+             if($this->session->getSessionData(strtolower($player->getName()), 'stat') == 5){
+                 $this->session->createSession(strtolower($player->getName()), 'min', $block->getX().":".$block->getY().":".$block->getZ());
+                 $this->session->createSession(strtolower($player->getName()), 'stat', 6);
+                 $player->sendMessage(ITransfer::Prfix.'Успешно. Теперь отмете вторую точку');
+             } else if($this->session->getSessionData(strtolower($player->getName()), 'stat') == 6){
+                $this->session->createSession(strtolower($player->getName()), 'max', $block->getX().":".$block->getY().":".$block->getZ());
+                $player->sendMessage(ITransfer::Prfix.'Успешно. Теперь напишите IP адрес. Порт после ":" Пример: 0.0.0.0:19132');
+                $this->session->createSession(strtolower($player->getName()), 'stat', 7);
+             }
          
+       }
+       public function cheakXYZ($x, $y, $z, $min, $max) {
+        /* @var $pos1 array */
+        $pos1 = explode(':', $min);
+        /* @var $pos2 array */
+        $pos2 = explode(':', $max);
+          //Топ код АЙДАНА --Начало--
+           $minX = min($pos1[0], $pos2[0]);//0 - x; 1 - y; 2 - z
+           $maxX = max($pos1[0], $pos2[0]); 
+            $minY = min($pos1[1], $pos2[1]);
+            $maxY = max($pos1[1], $pos2[1]);
+            $minZ = min($pos1[2], $pos2[2]);
+            $maxZ = max($pos1[2], $pos2[2]);
+            if ($minX <= $x and $x <= $maxX) {
+        if ($minY <= $y and $y <= $maxY) {
+        if ($minZ <= $z and $z <= $maxZ) {
+            return true;
+} else {
+return false;
+}
+} else {
+return false;
+}
+} else {
+return false;
+}
+//Топ код АЙДАНА --Конец--
        }
        public function signTask() {
 
@@ -155,7 +225,7 @@ class ITransfer extends PluginBase implements Listener {
            $server = explode(":", $addres);
            $ip = $server[0];
            $port = $server[1];
-           $data = json_decode(Utils::getURL("https://status.infomcpe.ru/query.php?ip={$ip}&port={$port}"), ture);
+           $data = json_decode(Utils::getURL("https://status.infomcpe.ru/query.php?ip={$ip}&port={$port}"), true);
            if($data['error'] == NULL){
                return $data[$id];
            } elseif ($data['error'] != NULL) {
@@ -175,15 +245,15 @@ class ITransfer extends PluginBase implements Listener {
            $pk->address = $ip;
            $player->dataPacket($pk);
        }
-       	public function dataSave($id, $tip, $data){
-  $Sfile = (new Config($this->getDataFolder() . "signs/".strtolower($id).".json", Config::JSON))->getAll();
+       	public function dataSave($id, $tip, $data, $type = 'signs'){
+  $Sfile = (new Config($this->getDataFolder() . $type."/".strtolower($id).".json", Config::JSON))->getAll();
   $Sfile[$tip] = $data;
-  $Ffile = new Config($this->getDataFolder() . "signs/".strtolower($id).".json", Config::JSON);
+  $Ffile = new Config($this->getDataFolder() . $type."/".strtolower($id).".json", Config::JSON);
   $Ffile->setAll($Sfile);
   $Ffile->save();
 }
-	 public function dataGet($id, $tip){
-   $Sfile = (new Config($this->getDataFolder() . "signs/".strtolower($id).".json", Config::JSON))->getAll();
+	 public function dataGet($id, $tip ,$type = 'signs'){
+   $Sfile = (new Config($this->getDataFolder() . $type."/".strtolower($id).".json", Config::JSON))->getAll();
    return $Sfile[$tip];
          }
       public function languageInitialization(){
